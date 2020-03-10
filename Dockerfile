0979c7d3d1f39790d4ad68e826a77f3a22f564a4
tthebst <gretler.tim@gmail.com>
2020-03-10 20:30:34 +0100

    added first tests

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ EXPOSE 80
 COPY . /app
 WORKDIR /app
 
-CMD [ "gunicorn", "-w","2", "--threads" ,"4" ,"-b", "0.0.0.0:80","app:app" ]
+#CMD [ "gunicorn", "-w","2", "--threads" ,"4" ,"-b", "0.0.0.0:80","app:app" ]
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app
--- app.py
+++ app.py
@@ -54,14 +54,11 @@ def get_txt_aws(filename):
     obj = s3.download_fileobj(Bucket="my-personal-website-tim", Key=filename, Fileobj=bytes_buffer)
     byte_value = bytes_buffer.getvalue()
     str_value = byte_value.decode()
-    print(str_value)
     return str_value
 
 
 def get_s3_client():
     s3 = boto3.client('s3',
-                      aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-                      aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
                       config=Config(signature_version='s3v4'),
                       region_name='eu-central-1'
                       )
@@ -73,5 +70,4 @@ def get_signed_url(location):
     url = s3.generate_presigned_url(
         ClientMethod="get_object", Params={"Bucket": "my-personal-website-tim", "Key": location}
     )
-    print(url)
     return url
