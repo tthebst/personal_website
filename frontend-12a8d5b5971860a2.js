96f3f2fd767db06ba524c20cd2787431ccc3c922
tthebst <tthebst@users.noreply.github.com>
2022-05-02 18:25:17 +0000

    deploy: c0a9db2f7b4f706621de20cae7be18b8483e6bfb

--- frontend-12a8d5b5971860a2.js
+++ frontend-12a8d5b5971860a2.js
@@ -310,7 +310,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('frontend-8056a758d0c65e99_bg.wasm', import.meta.url);
+        input = new URL('frontend-12a8d5b5971860a2_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
